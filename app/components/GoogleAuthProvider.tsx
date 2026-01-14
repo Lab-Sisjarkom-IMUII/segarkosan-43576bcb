4f8f1e439e2907894c1b736a3bd1642add71b73b
ZanDev32 <52073144+ZanDev32@users.noreply.github.com>
2026-01-15 00:26:29 +0700

    hardcode the client id cuz imuii cannot save import .env correctly

--- app/components/GoogleAuthProvider.tsx
+++ app/components/GoogleAuthProvider.tsx
@@ -7,7 +7,8 @@ export default function GoogleAuthProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || 
+    "1097843415098-ld0g5r4in2acqiom02rghkt5juj5poht.apps.googleusercontent.com";
 
   if (!clientId) {
     console.error("Google Client ID is missing. Please set NEXT_PUBLIC_GOOGLE_CLIENT_ID in your environment variables.");
